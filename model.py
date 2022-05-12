28d2e528d7e20b3cabe04ba860c0d6ff1142a7cc
Jang Wonbeom <jtiger958@gmail.com>
2022-05-12 12:08:16 +0900

    add vgg, resnet, regnet

--- model.py
+++ model.py
@@ -3,14 +3,32 @@ import torch.nn as nn
 import torchvision.models as models
 
 model_map = dict(
-    vgg16=models.vgg16(pretrained=True),
-    resnet=models.resnet18(pretrained=True),
-    resnet=models.resnet18(pretrained=True),
-    resnet=models.resnet18(pretrained=True),
-    resnet=models.resnet18(pretrained=True),
+    vgg16=models.vgg16,
+    resnet18=models.resnet18,
+    resnet34=models.resnet34,
+    resnet50=models.resnet50,
+    resnet101=models.resnet101,
+    regnet=models.regnet_y_3_2gf
 )
 
+
 class Model(nn.Module):
-    def __init__(self):
+    def __init__(self, backbone="regnet", num_classes=10):
         super(Model, self).__init__()
-        backbone
+        backbone = model_map[backbone]
+
+        if isinstance(backbone, models.ResNet) or isinstance(backbone, models.RegNet):
+            backbone.fc = nn.Linear(512, num_classes)
+        elif isinstance(backbone, models.VGG):
+            backbone.classifier = nn.Sequential(
+                nn.Linear(512 * 7 * 7, 4096),
+                nn.ReLU(True),
+                nn.Dropout(),
+                nn.Linear(4096, 4096),
+                nn.ReLU(True),
+                nn.Dropout(),
+                nn.Linear(4096, num_classes),
+            )
+
+    def forward(self, x):
+        return self.backbone(x)
