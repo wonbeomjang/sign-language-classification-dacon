383053046ea720c8c272f53ef2b50db498935760
Wonbeom Jang <jtiger958@gmail.com>
2022-05-16 20:04:56 +0900

    Update train.py

--- train.py
+++ train.py
@@ -130,5 +130,5 @@ def _train():
 if __name__ == "__main__":
     sweep_id = wandb.sweep(get_sweep_config())
     count = 5
-    wandb.agent(sweep_id, function=_train, count=count, project='sign_language')
+    wandb.agent(sweep_id, function=_train, count=count)
 
