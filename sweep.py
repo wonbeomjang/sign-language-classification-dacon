4ae0720c550bf51b18125c2db18651ec8304b07b
Wonbeom Jang <jtiger958@gmail.com>
2022-05-17 16:59:39 +0900

    Update sweep.py

--- sweep.py
+++ sweep.py
@@ -3,5 +3,5 @@ from config import get_sweep_config
 
 if __name__ == "__main__":
     sweep_id = wandb.sweep(get_sweep_config())
-    count = 5
+    count = 50
     wandb.agent(sweep_id, function=_train, count=count)
