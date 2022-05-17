15d48ff9eb68d9063c06d081d97d807898bc7d33
Jang Wonbeom <jtiger958@gmail.com>
2022-05-17 15:02:13 +0900

    add start.sh

--- test.py
+++ test.py
@@ -47,3 +47,20 @@ if __name__ == "__main__":
     result = test(net, data_loader, config)
     df = pd.DataFrame.from_dict(result)
     df.to_csv("submission.csv", index=False)
+
+    out = []
+    with open(f"submission.csv") as f:
+        for line in f.readlines():
+            file_name, index = line.split(',')
+            if index == "0\n":
+                index = "10-1\n"
+            elif index == "10\n":
+                index = "10-2\n"
+
+            out += [f"{file_name},{index}"]
+
+    with open(f"submission.csv", "w") as f:
+        f.write("".join(out))
+
+
+
